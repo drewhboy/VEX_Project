dfae66a68109bfe6625f53858fb6b86e8e74c083
Andrew Hideo Boyles <73495888+drewhboy@users.noreply.github.com>
2022-01-13 21:47:32 -0800

    Update extract_anim.py
    divide_by_transform

--- extract_anim.py
+++ extract_anim.py
@@ -1,8 +1,21 @@
 def create_main_subnet(alembic_input):
     subnet_node = hou.node('/obj').createNode('subnet', 'extract_animation')
     subnet_node.moveToGoodPosition()
-    divide_geo_node = subnet_node.createNode('geo', 'divide_into_parts')
+    return subnet_node
+
+def create_divide_into_parts_geo_node(parent_node):
+    divide_geo_node = parent_node.createNode('geo', 'divide_into_parts')
     divide_geo_node.moveToGoodPosition()
+    return divide_geo_node
+
+def divide_into_parts(parent_node, alembic_path):
     obj_merge = divide_geo_node.createNode('object_merge', 'merge_alembic')
     obj_merge.parm('objpath1').set(alembic_input)
     obj_merge.moveToGoodPosition()
+    return obj_merge
+
+
+def main(alembic_path):
+    subnet = create_main_subnet()
+    divide_node = create_divide_into_parts_geo_node(subnet)
+    divide_into_parts(divide_node, alembic_path)
