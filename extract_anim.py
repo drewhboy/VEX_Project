6de0468ef43580454dbaa848c3bc0b5574dddc92
Andrew Hideo Boyles <73495888+drewhboy@users.noreply.github.com>
2022-01-15 19:34:06 -0800

    updates to extract anim

--- extract_anim.py
+++ extract_anim.py
@@ -1,11 +1,19 @@
-def create_main_subnet(alembic_input):
-    subnet_node = hou.node('/obj').createNode('subnet', 'extract_animation')
+import hou
+
+static_parts = {}
+
+def create_main_subnet(parent_node, node_name=''):
+    if node_name:
+        subnet_node = parent_node.createNode('subnet', node_name)
+    else:
+        subnet_node = parent_node.createNode('subnet')
     subnet_node.moveToGoodPosition()
     return subnet_node
 
 def create_divide_into_parts_geo_node(parent_node):
     divide_geo_node = parent_node.createNode('geo', 'divide_into_parts')
     divide_geo_node.moveToGoodPosition()
+    divide_geo_node.setDisplayFlag(0)
     return divide_geo_node
 
 def divide_into_parts(parent_node, alembic_path):
@@ -51,17 +59,63 @@ def create_outputs(parent_node, input_node):
     parts = {}
 
     for part in range(total_parts):
-        part += 1
+        part += 1 
         blast_node = blast_all_except_current(parent_node, input_node, attribute_name, part)
         static_output = create_static_output(parent_node, blast_node, part)
         anim_output = create_anim_output(parent_node, blast_node, part)
         parts[part] = [static_output, anim_output]
     return parts
 
-    
+def extract_anim(parent_node, moving_parts):
+    for part in moving_parts.keys():
+        extract_geo = parent_node.createNode('extractgeo', 'extract_part_{0}'.format(part))
+        extract_geo.moveToGoodPosition()
+        extract_geo.parm('srcpath').set(moving_parts[part][0].path())
+        extract_geo.parm('dstpath').set(moving_parts[part][1].path())
+        static_geo = parent_node.createNode('geo', 'part_{0}'.format(part))
+        object_merge = static_geo.createNode('object_merge')
+        object_merge.moveToGoodPosition()
+        object_merge.parm('objpath1').set(moving_parts[part][0].path())
+        static_geo.setInput(0, extract_geo)
+        static_geo.moveToGoodPosition(move_inputs=False)
+        static_null = static_geo.createNode('null', 'OUT')
+        static_null.setInput(0, object_merge)
+        static_null.moveToGoodPosition(move_inputs=False)
+        static_null.setDisplayFlag(1)
+        static_parts[part] = static_null
 
-def main(alembic_path):
-    subnet = create_main_subnet(alembic_path)
+def create_packed_geo_dops(parent_node):
+    packed_nodes = []
+    for part in static_parts.keys():
+        packed_geo = parent_node.createNode('rbdpackedobject', 'part_{0}'.format(part))
+        packed_geo.moveToGoodPosition()
+        packed_geo.parm('soppath').set(static_parts[part].path())
+        packed_geo.setParms({'initialstate':2, 'usetransform':1})
+        packed_nodes.append(packed_geo)
+    return packed_nodes
+
+def merge_packed_nodes(parent_node, nodes_to_merge):
+    merge = parent_node.createNode('merge')
+    for node in nodes_to_merge:
+        merge.setNextInput(node)
+    merge.moveToGoodPosition(move_inputs=False)
+    return merge
+
+def create_output(parent_node, input_node):
+    output = parent_node.createNode('output')
+    output.setInput(0, input_node)
+    output.moveToGoodPosition(move_inputs=False)
+    output.setDisplayFlag(True)
+
+def extract(alembic_path):
+    subnet = create_main_subnet(hou.node('/obj'), 'extract_animation')
     divide_node = create_divide_into_parts_geo_node(subnet)
     divide_hda = divide_into_parts(divide_node, alembic_path)
-    create_outputs(divide_node, divide_hda)
+    parts = create_outputs(divide_node, divide_hda)
+    extract_anim(subnet, parts)
+
+def create_collisions(dop_path):
+    subnet = create_main_subnet(hou.node(dop_path), 'collisions')
+    packed_nodes = create_packed_geo_dops(subnet)
+    merge_node = merge_packed_nodes(subnet, packed_nodes)
+    create_output(subnet, merge_node)
